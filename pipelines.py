4fc29bbcd473dbad84363c7508b1b56a3e4d2b7d
Yunhan <yunhanyu@usc.edu>
2017-05-19 23:30:27 -0700

    mongdb

---
+++ pipelines.py
@@ -0,0 +1,14 @@
+import pymongo
+
+from scrapy.conf import settings
+
+
+class MongoDBPipeline(object):
+
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
